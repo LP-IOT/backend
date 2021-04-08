529fc473e08194e2535e1957011969ee9104f519
David Ben Khemis <david.benkhemis@hospitalink.fr>
2021-04-08 15:07:35 +0200

    init entity

---
+++ src/app/etudiant/etudiant.entity.ts
@@ -0,0 +1,11 @@
+import { Column, Entity } from 'typeorm';
+import { BaseFieldEntity } from '../utils/baseEntity.entity';
+
+@Entity()
+export class Etudiant extends BaseFieldEntity {
+
+  @Column('text', { nullable: false })
+  nom: string;
+
+  
+}
---
+++ src/app/notes/notes.entity.ts
@@ -0,0 +1,10 @@
+import { Column, Entity } from 'typeorm';
+import { BaseFieldEntity } from '../utils/baseEntity.entity';
+
+@Entity()
+export class Note extends BaseFieldEntity {
+
+  @Column('number', { nullable: false })
+  montant: number;
+
+}
