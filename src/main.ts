271ab2d2c5253d242d50db877536b504d4c4fe4d
David Ben Khemis <david.benkhemis@hospitalink.fr>
2021-04-13 16:33:30 +0200

    add api prefix on swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
@@ -7,11 +7,14 @@ global['fetch'] = require('node-fetch');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  
   const config = new DocumentBuilder()
     .setTitle('CDI Rest API')
     .setDescription('Here are the link to the Rest API')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
@@ -19,7 +22,7 @@ async function bootstrap() {
     origin: false,
     credentials: false,
   });
-  app.setGlobalPrefix('api');
+
   Logger.log('Listened on PORT 3000');
   await app.listen(process.env.PORT || 3000);
 }
