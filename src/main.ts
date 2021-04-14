1c931fcfef3b4c297bf33a9d3a81dbb1bee01d52
Adrian-Paul Carrières <36892477+AdrianPaulCarrieres@users.noreply.github.com>
2021-04-14 10:48:23 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: 'http://adrianpaul-carrieres.fr:4200',
     credentials: false,
   });
 
